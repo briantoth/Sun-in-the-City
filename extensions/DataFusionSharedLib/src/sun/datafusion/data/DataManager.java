06392df403c2d3e1f904cfbc43a08c3be10da7af
Phillip Tischler <pmt43@cornell.edu>
2012-10-30 00:12:18 -0400

    Setup skeleton for the shared library

--- extensions/DataFusionSharedLib/src/sun/datafusion/data/DataManager.java
+++
@@ -1,9 +0,0 @@
-package sun.datafusion.data;
-
-/*******************************************************************************
- * Manages the interaction with data
- */
-public class DataManager {
-	// TODO
-
-}
