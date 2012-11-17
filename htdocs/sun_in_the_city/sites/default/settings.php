0fefd63912e5784ec8efb3e4693996df1de16897
Phillip Tischler <pmt43@cornell.edu>
2012-11-17 14:43:53 -0500

    Merging problems. Going to have to redo CSS

--- htdocs/sun_in_the_city/sites/default/settings.php
+++ htdocs/sun_in_the_city/sites/default/settings.php
@@ -246,7 +246,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = 'MZ3NcjXvyZZkieUqjzdulNuWlXfMMmKqm1IOuYQZJ3g';
+$drupal_hash_salt = 'mtKbPfAkTrnqfgd6qiV_UM8GL3jHFOusy6HOrDtjFB8';
 
 /**
  * Base URL (optional).
