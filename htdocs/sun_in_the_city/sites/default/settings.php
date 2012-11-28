72fc4a86aa24c21bb6b3e3eb7bcc0ca1df182835
Phillip Tischler <pmt43@cornell.edu>
2012-11-27 21:46:50 -0500

    Updated doc

--- htdocs/sun_in_the_city/sites/default/settings.php
+++ htdocs/sun_in_the_city/sites/default/settings.php
@@ -207,7 +207,7 @@ $databases = array (
     array (
       'database' => 'sun_in_the_city',
       'username' => 'root',
-      'password' => 'lin',
+      'password' => 'root',
       'host' => 'localhost',
       'port' => '',
       'driver' => 'mysql',
@@ -246,7 +246,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = 'B4nMA4PcT6vYAgtAHGMXSL-I5DMDA5DaYuYESBKwxew';
+$drupal_hash_salt = 'HeFR2DuF5JQnafbbIKP9C0vOh9MDRGiIdgjG50wnmbQ';
 
 /**
  * Base URL (optional).
